2bad82b1f915ead9b7bbca3c84105d325ca2e34e
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2021-08-15 12:53:13 +0200

    Add stylelint for sass

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-sass-guidelines', 'stylelint-config-standard'],
   rules: {
-    'at-rule-no-unknown': null
+    'at-rule-no-unknown': null,
+    'selector-class-pattern': null,
+    'selector-max-id': 10,
+    'max-nesting-depth': 4,
   }
 }
