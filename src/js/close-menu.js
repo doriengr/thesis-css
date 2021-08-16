df228ce49f95f2c2e78ce50fabe1c352b3c744bf
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2021-08-16 13:55:19 +0200

    Add js files

---
+++ src/js/close-menu.js
@@ -0,0 +1,16 @@
+(function () {
+  const navItems = document.querySelectorAll('.js-link');
+  const checkbox = document.getElementById('navigation-open');
+  
+  function closeMenu() {
+    for(let i = 0; i < navItems.length; i++) {
+      navItems[i].addEventListener('click', () => {
+        if(window.matchMedia('(max-width: 1024px)').matches) {
+          checkbox.checked = false;
+        }
+      });
+    }
+  }
+  
+  closeMenu();
+})();
